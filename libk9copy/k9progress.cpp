a6821ea0c3212169ca638cb680fd8f0bd6efce3b
jnmchlptt <jnmchlptt@250c61c0-cc0d-0410-af45-d4f8fccfe43b>
2006-04-06 06:20:03 +0000

    git-svn-id: https://k9copy.svn.sourceforge.net/svnroot/k9copy/trunk@38 250c61c0-cc0d-0410-af45-d4f8fccfe43b

---
+++ libk9copy/k9progress.cpp
@@ -0,0 +1,62 @@
+//
+// C++ Implementation: 
+//
+// Description: 
+//
+//
+// Author: Jean-Michel PETIT <k9copy@free.fr>, (C) 2006
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+
+
+#include "k9progress.h"
+#include <kprogress.h>
+#include <qlabel.h>
+#include <qapplication.h>
+
+k9Progress::k9Progress(QWidget* parent, const char* name, bool modal, WFlags fl)
+: Progress(parent,name, modal,fl)
+{
+  m_process=new QProcess();
+  connect(m_process, SIGNAL(processExited()), this, SLOT(accept()));
+}
+
+k9Progress::~k9Progress()
+{
+}
+
+
+void k9Progress::setLabelText(const QString _text) {
+   LabelText->setText(_text);
+}
+
+void k9Progress::setProgress(long _position,long _total) {
+   ProgressBar->setRange(0,_total);
+   ProgressBar->setProgress(_position);
+}
+
+int k9Progress::execute() {
+   if (!m_process->start())
+	return -1;
+   if (exec()==QDialog::Rejected)
+   	return 0;
+   else
+	return 1;
+}
+
+
+void k9Progress::bCancelClick() {
+   m_process->tryTerminate();
+   reject();
+}
+
+QProcess* k9Progress::getProcess() const {
+    return m_process;
+}
+
+/*$SPECIALIZATION$*/
+
+
+#include "k9progress.moc"
---
+++ libk9copy/k9progress.h
@@ -0,0 +1,47 @@
+//
+// C++ Interface: 
+//
+// Description: 
+//
+//
+// Author: Jean-Michel PETIT <k9copy@free.fr>, (C) 2006
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+
+#ifndef K9PROGRESS_H
+#define K9PROGRESS_H
+
+#include "k9common.h"
+#include "progress.h"
+#include <qprocess.h>
+
+class k9Progress : public Progress
+{
+  Q_OBJECT
+
+public:
+  k9Progress(QWidget* parent = 0, const char* name = 0, bool modal = FALSE, WFlags fl = 0 );
+  ~k9Progress();
+
+   QProcess* getProcess() const;
+   void setProgress(long _position,long _total);
+   void setLabelText(const QString _text);
+   int execute();
+  /*$PUBLIC_FUNCTIONS$*/
+
+public slots:
+  /*$PUBLIC_SLOTS$*/
+
+protected:
+  /*$PROTECTED_FUNCTIONS$*/
+  QProcess *m_process;
+  void bCancelClick();
+  
+protected slots:
+  /*$PROTECTED_SLOTS$*/
+};
+
+#endif
+
