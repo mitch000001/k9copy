d4829db5ac48daf00e94a81709a5776a70e10524
jnmchlptt <jnmchlptt@250c61c0-cc0d-0410-af45-d4f8fccfe43b>
2006-10-31 22:57:31 +0000

    git-svn-id: https://k9copy.svn.sourceforge.net/svnroot/k9copy/trunk@129 250c61c0-cc0d-0410-af45-d4f8fccfe43b

---
+++ k9Mplayer/k9mplayer.cpp
@@ -0,0 +1,243 @@
+
+#include "k9common.h"
+#include <qlayout.h>
+#include <qslider.h>
+#include <kselect.h>
+#include "k9mplayer.h"
+#include <qcombobox.h>
+#include <kiconloader.h>
+#include <kpushbutton.h>
+#include "k9dvd.h"
+#include "k9dvdtitle.h"
+#include <kprocess.h>
+#include <qlabel.h>
+#include <qtimer.h>
+#include <qapplication.h>
+#include <qevent.h>
+#include <ksimpleconfig.h>
+
+K9Mplayer::K9Mplayer(QObject  *parent,const char *name,const QStringList args):MPlayer((QWidget*)parent){
+   m_seeking=FALSE;
+   m_initVol=TRUE;
+   m_title=0;
+
+   m_process=new KProcess(this);
+   m_ratio=4.0/3.0;
+
+   slider->setRange(0, 100);
+   slider->setSteps(1, 1);
+    bPlay->setPixmap(SmallIcon("player_play"));
+    bStop->setPixmap(SmallIcon("player_stop"));
+    connect(m_process,SIGNAL(receivedStdout( KProcess*, char*, int )),this,SLOT(receivedStdout( KProcess*, char*, int )));
+    m_timer=new QTimer(this);
+    connect(m_process,SIGNAL(wroteStdin( KProcess* )),this,SLOT(wroteStdin( KProcess* )));
+    connect(m_timer,SIGNAL(timeout()),this,SLOT(timeout()));
+    m_container=new QWidget(Label);
+}
+
+
+K9Mplayer::~K9Mplayer()
+{
+}
+
+void K9Mplayer::resizeEvent ( QResizeEvent * _resizeEvent) {
+  int w,h;
+  w=Label->width();
+  h=Label->width() /m_ratio;
+  if (h>Label->height()) {
+	h=Label->height();
+	w=Label->height()*m_ratio;
+  } 
+
+   m_container->resize(w,h);
+   m_container->move((Label->width()-w)/2,(Label->height()-h)/2);
+}
+
+
+void K9Mplayer::wroteStdin( KProcess *_process) {
+   m_canwrite = TRUE;
+}
+
+void K9Mplayer::timeout() {
+   sendCmd( "get_percent_pos");
+
+}
+
+void K9Mplayer::setDevice(const QString & _device) {
+//    m_xineWidget->slotSetDvdDevice( _device);
+    m_device=_device;
+}
+
+
+void K9Mplayer::setTitle( const QString & _numTitle) {
+/*   m_xineWidget->clearQueue();
+   m_xineWidget->appendToQueue(QString("dvd:/%1").arg(_numTitle));
+   m_xineWidget->slotPlay();
+*/
+   if (!m_process->isRunning()) {
+	KSimpleConfig settings("K9Copy");
+	QString vout[]= {"x11","xv","gl2","sdl"};
+	QString aout[]= {"alsa","oss","sdl"};
+	QString vo=vout[settings.readEntry("/mplayer/vout",0).toInt()];
+    	QString ao=aout[settings.readEntry("/mplayer/aout",0).toInt()];
+
+
+
+	*m_process << "mplayer"; 
+	 *m_process << "-vo" << vo << "-ao" << ao << "-sws" << "0"; 
+	*m_process << "-framedrop";
+	*m_process << "-wid" << QString::number(m_container->winId()) ;
+	*m_process << "-slave" ;
+	*m_process <<  "-idle";
+	*m_process << "-dvd-device" << m_device;
+	m_process->start( KProcess::NotifyOnExit,KProcess::All);
+	m_canwrite=TRUE;
+   }
+   sendCmd( QString("loadfile dvd://%1").arg(_numTitle));
+   m_position=0;
+   slider->setValue(m_position);
+}
+
+
+
+
+/*$SPECIALIZATION$*/
+
+void  K9Mplayer::receivedStdout (KProcess *proc, char *buffer, int buflen) {
+   QString buf = QString::fromLatin1(buffer, buflen);
+   sscanf(buf.latin1(),"ANS_PERCENT_POSITION=%d",&m_position);
+   if (!m_seeking && m_position>0) {
+   	slider->setValue(m_position);
+   }
+/*
+   int ipos=buf.find("ANS_PERCENT_POSITION=");
+   if (ipos>=0 && !m_seeking) {
+	QString sPosition=buf.mid(ipos).section("=",1,1);
+        qDebug(buf);
+
+	m_position=sPosition.toInt();
+	slider->setValue(m_position);
+   }
+*/
+
+}
+
+void K9Mplayer::slotLengthChanged() {
+//	slider->setEnabled(m_xineWidget->isSeekable());
+//	slider->setValue(m_xineWidget->getPosition());
+}
+
+void K9Mplayer::slotNewPosition(int _pos,const QTime & _time) {
+	slider->setValue(_pos);
+
+}
+
+void K9Mplayer::sliderReleased()
+{
+     sendCmd( QString("seek %1 1").arg(slider->value()));
+     m_seeking=FALSE;
+}
+
+void K9Mplayer::sliderChanged( int _value) {
+  
+}
+
+void K9Mplayer::sliderPressed() {
+  m_seeking=TRUE;
+//  m_xineWidget->slotStartSeeking();
+
+}
+
+void K9Mplayer::bPlayClick()
+{
+   setTitle(QString::number( m_title));
+}
+
+void K9Mplayer::bStopClick()
+{
+	sendCmd("quit");
+}
+
+
+void K9Mplayer::bDownClick() {
+	sendCmd( QString("volume -1"));
+ 
+}
+
+void K9Mplayer::bUpClick() {
+	sendCmd( QString("volume +1"));
+ 
+}
+
+
+void K9Mplayer::open( k9DVD *_dvd,k9DVDTitle *_title) {
+    cbSub->clear();
+    cbAudio->clear();
+    for (int i=0; i< _title->getaudioStreamCount();i++)
+	cbAudio->insertItem("");
+    for (int i=0; i< _title->getaudioStreamCount();i++) {
+	k9DVDAudioStream *aud=_title->getaudioStream(i);
+	cbAudio->changeItem(aud->getlanguage(),aud->getID()-1);
+    }
+
+    for (int i=0; i< _title->getsubPictureCount();i++) 
+	cbSub->insertItem("");
+    for (int i=0; i< _title->getsubPictureCount();i++) {
+	k9DVDSubtitle *sub=_title->getsubtitle( i);
+	cbSub->changeItem(sub->getlanguage(),sub->getID()-1);
+    }
+
+    if(_title->getaspectRatio()=="16:9")
+	m_ratio=16.0/9.0;
+    else
+        m_ratio=4.0/3.0;
+    resizeEvent( NULL);
+
+    setDevice( _dvd->getDevice());
+    m_dvdTitle=_title;
+    m_title=_title->getnumTitle();
+    setTitle( QString::number(m_title));
+    if (_title->getaudioStreamCount() >0)
+    	cbAudioActivated( 0);
+    if (_title->getsubPictureCount() >0)
+    	cbSubActivated( 0);
+    if (!m_timer->isActive())
+    	m_timer->start(1000,FALSE);
+
+}
+
+void K9Mplayer::titleChanged() {
+//  int currentTitle=m_xineWidget->currentDVDTitleNumber();
+//  if (m_title!=currentTitle)
+//      m_xineWidget->slotStop();
+
+}
+
+void K9Mplayer::cbAudioActivated( int _value) {
+ //   m_xineWidget->slotSetAudioChannel( _value +1 );
+    int value=m_dvdTitle->getaudioStream( _value)->getStreamId();
+    sendCmd(QString( "switch_audio %1").arg(value));
+}
+
+void K9Mplayer::cbSubActivated( int _value) {
+ //  m_xineWidget->slotSetSubtitleChannel( _value +1);
+    sendCmd(QString("sub_select %1").arg(_value));
+ 
+}
+
+
+void K9Mplayer::sendCmd( QString _cmd) {
+    while (!m_canwrite) {
+       qApp->processEvents();
+       if ( !m_process->isRunning())
+	    return;
+    }
+	
+    _cmd +="\n"; 
+    m_canwrite=FALSE;
+    m_process->writeStdin( _cmd.latin1() ,_cmd.length());
+
+}
+
+#include "k9mplayer.moc"
+
---
+++ k9Mplayer/k9mplayer.h
@@ -0,0 +1,65 @@
+
+#ifndef K9Mplayer_H
+#define K9Mplayer_H
+
+#include "k9common.h"
+#include "../k9Mplayer/mplayer.h"
+
+
+class k9DVD;
+class k9DVDTitle;
+class KProcess;
+class QTimer;
+class QResizeEvent;
+
+class K9Mplayer : public MPlayer
+{
+  Q_OBJECT
+private:
+  KProcess    *m_process;
+  bool m_seeking,m_initVol;
+  int m_title;
+  k9DVDTitle *m_dvdTitle;
+  QString m_device;
+  QTimer *m_timer;
+  void sendCmd(QString _cmd);
+  int m_position;
+  bool m_canwrite;
+  QWidget *m_container;
+  float m_ratio;
+public:
+  //K9Mplayer(QWidget* parent = 0, const char* name = 0, WFlags fl = 0 );
+  K9Mplayer(QObject  *parent=0,const char *name=0,const QStringList args=0);
+  ~K9Mplayer();
+  /*$PUBLIC_FUNCTIONS$*/
+  void setDevice(const QString & _device);
+  void setTitle(const QString & _numTitle);
+private slots:
+  void slotLengthChanged();
+  void slotNewPosition(int _pos,const QTime & _time);
+  void receivedStdout (KProcess *proc, char *buffer, int buflen);
+  void wroteStdin(KProcess *_process);
+  void timeout();
+public slots:
+  /*$PUBLIC_SLOTS$*/
+  virtual void          sliderReleased();
+  virtual void          bPlayClick();
+  virtual void          bStopClick();
+  virtual void 		sliderPressed();
+  virtual void		sliderChanged(int _value);
+  virtual void 		open(k9DVD *_dvd,k9DVDTitle *_title);
+  virtual void		titleChanged();
+  virtual void		cbAudioActivated (int _value);
+  virtual void 		cbSubActivated (int _value);
+  virtual void		bUpClick();
+  virtual void 		bDownClick();
+protected:
+  /*$PROTECTED_FUNCTIONS$*/
+  void resizeEvent ( QResizeEvent * _resiseEvent);
+protected slots:
+  /*$PROTECTED_SLOTS$*/
+
+};
+
+#endif
+
