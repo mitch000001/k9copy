e0fae2449c6a06a76915076266270276d541333c
jnmchlptt <jnmchlptt@250c61c0-cc0d-0410-af45-d4f8fccfe43b>
2007-01-16 21:36:28 +0000

    git-svn-id: https://k9copy.svn.sourceforge.net/svnroot/k9copy/trunk@152 250c61c0-cc0d-0410-af45-d4f8fccfe43b

---
+++ libk9copy/k9config.cpp
@@ -0,0 +1,114 @@
+//
+// C++ Implementation: k9config
+//
+// Description: 
+//
+//
+// Author: Jean-Michel PETIT <k9copy@free.fr>, (C) 2007
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#include "k9config.h"
+
+k9Config::k9Config() {
+    m_config=new KSimpleConfig("K9Copy");
+    read();
+}
+
+void k9Config::read() {
+  // if (! m_config->hasGroup( "options"))
+  // 	readOldConfig();
+  m_config->setGroup( "dir");
+  m_prefOutput=m_config->readEntry("output",locateLocal("tmp","k9copy/",true));
+  
+  m_config->setGroup("devices");
+  m_InputDev=m_config->readNumEntry("input",0);
+  m_OutputDev=m_config->readNumEntry("output",0);
+
+  m_config->setGroup("options");
+  m_prefK3b=m_config->readBoolEntry("usek3b",false);
+  m_prefAutoBurn=m_config->readBoolEntry("autoburn",false);
+  m_quickScan=m_config->readBoolEntry("quickscan",true);
+  m_prefSize=m_config->readNumEntry("dvdsize",4400);
+  m_useDvdAuthor=m_config->readBoolEntry("useDvdAuthor",true);
+  m_keepMenus=m_config->readBoolEntry("keepMenus",true);
+
+  m_config->setGroup("mp4");
+  m_prefMp4Codec=m_config->readNumEntry("codec",0);
+  m_prefMp4Size=m_config->readNumEntry("size",700);
+  m_prefMp4NumberCD=m_config->readNumEntry("numberCD",1);
+  m_prefMp4Width=m_config->readEntry("width","640");
+  m_prefMp4Height=m_config->readEntry("height","");
+  m_prefMp4AspectRatio=m_config->readBoolEntry("aspectRatio",true);
+  m_prefMp42Passes=m_config->readBoolEntry("2passes",false);
+  m_prefMp4AudioBitrate=m_config->readEntry("audiobitrate","128");
+
+  m_config->setGroup("mencoder");
+  m_codecAudio=m_config->readListEntry("audio");
+  m_codecLabels=m_config->readListEntry("labels");
+  m_codecVideo=m_config->readListEntry("video");
+  
+  m_config->setGroup("devices");
+  m_devices=m_config->readListEntry("dev");
+  m_devicesLabels=m_config->readListEntry("labels");
+  m_devicesIO=m_config->readListEntry("IO");
+
+  m_config->setGroup("preview");
+  m_useMplayer=m_config->readBoolEntry("userMplayer",false);
+  m_useGL=m_config->readBoolEntry("useGL",false);
+  m_MplayerVout=m_config->readNumEntry("MplayerVout",0);
+  m_MplayerAout=m_config->readNumEntry("MplayerAout",0);
+}
+
+void k9Config::save() {
+  m_config->setGroup( "dir");
+  m_config->writeEntry("output",m_prefOutput);
+  
+  m_config->setGroup("devices");
+  m_config->writeEntry("input", m_InputDev);
+  m_config->writeEntry("output",m_OutputDev);
+
+  m_config->setGroup("options");
+  m_config->writeEntry("usek3b",m_prefK3b);
+  m_config->writeEntry("autoburn",m_prefAutoBurn);
+  m_config->writeEntry("quickscan",m_quickScan);
+  m_config->writeEntry("dvdsize",m_prefSize);
+  m_config->writeEntry("useDvdAuthor",m_useDvdAuthor);
+  m_config->writeEntry("keepMenus",m_keepMenus);
+  
+  m_config->setGroup("mp4");
+  m_config->writeEntry("codec",m_prefMp4Codec);
+  m_config->writeEntry("size",m_prefMp4Size);
+  m_config->writeEntry("numberCD",m_prefMp4NumberCD);
+  m_config->writeEntry("width",m_prefMp4Width);
+  m_config->writeEntry("height",m_prefMp4Height);
+  m_config->writeEntry("audiobitrate",m_prefMp4AudioBitrate);
+  m_config->writeEntry("aspectRatio",m_prefMp4AspectRatio);
+  m_config->writeEntry("2passes",m_prefMp42Passes);
+
+  m_config->setGroup("mencoder");
+  m_config->writeEntry("audio",m_codecAudio);
+  m_config->writeEntry("labels",m_codecLabels);
+  m_config->writeEntry("video",m_codecVideo);
+
+  m_config->setGroup("devices");
+  m_config->writeEntry("dev",m_devices);
+  m_config->writeEntry("labels",m_devicesLabels);
+  m_config->writeEntry("IO",m_devicesIO);
+
+  m_config->setGroup("preview");
+  m_config->writeEntry("useMplayer",m_useMplayer);
+  m_config->writeEntry("useGL",m_useGL);
+  m_config->writeEntry("MplayerVout",m_MplayerVout);
+  m_config->writeEntry("MplayerAout",m_MplayerAout);
+
+}
+
+
+k9Config::~k9Config(){
+   delete m_config;
+
+}
+
+
---
+++ libk9copy/k9config.h
@@ -0,0 +1,351 @@
+//
+// C++ Interface: k9config
+//
+// Description: 
+//
+//
+// Author: Jean-Michel PETIT <k9copy@free.fr>, (C) 2007
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#ifndef K9CONFIG_H
+#define K9CONFIG_H
+#include "k9common.h"
+
+#include <ksimpleconfig.h>
+#include <kstandarddirs.h>
+
+/**
+	@author Jean-Michel PETIT <k9copy@free.fr>
+*/
+class k9Config{
+private:
+    KSimpleConfig *m_config;
+    int  m_InputDev;
+    int  m_OutputDev;
+    bool m_keepMenus;
+    QString m_prefOutput;
+    bool m_useDvdAuthor;
+    bool m_quickScan;
+    int  m_prefSize;
+    bool m_prefK3b;
+    bool m_prefMenu;
+    bool m_prefAutoBurn;
+    int  m_prefMp4Codec;
+    int  m_prefMp4Size;
+    int  m_prefMp4NumberCD;
+    bool m_prefMp4AspectRatio;
+    bool m_prefMp42Passes;
+    QString  m_prefMp4Width;
+    QString  m_prefMp4Height;
+    QString  m_prefMp4AudioBitrate;
+    QStringList m_codecAudio;
+    QStringList m_codecLabels;
+    QStringList m_codecVideo;
+    QString m_prefCodecAudio,m_prefCodecVideo,m_prefCodecLabel;
+
+    QStringList m_devices;
+    QStringList m_devicesLabels;
+    QStringList m_devicesIO;
+    
+    bool m_useMplayer;
+    bool m_useGL;
+    int  m_MplayerVout;
+    int m_MplayerAout;
+public:
+    k9Config();
+    void read();
+    void save();
+    ~k9Config();
+
+	void setInputDev(const int& _value) {
+	    m_InputDev = _value;
+	}
+	
+
+	int getInputDev() const {
+	    return m_InputDev;
+	}
+
+	void setOutputDev(const int& _value) {
+	    m_OutputDev = _value;
+	}
+	
+
+	int getOutputDev() const {
+	    return m_OutputDev;
+	}
+
+	void setKeepMenus(bool _value) {
+	    m_keepMenus = _value;
+	}
+	
+
+	bool getKeepMenus() const {
+	    return m_keepMenus;
+	}
+
+	void setPrefOutput(const QString& _value) {
+	    m_prefOutput = _value;
+	}
+	
+
+	QString getPrefOutput() const {
+	    return m_prefOutput;
+	}
+
+	void setUseDvdAuthor(bool _value) {
+	    m_useDvdAuthor = _value;
+	}
+	
+
+	bool getUseDvdAuthor() const {
+	    return m_useDvdAuthor;
+	}
+
+	void setQuickScan(bool _value) {
+	    m_quickScan = _value;
+	}
+	
+
+	bool getQuickScan() const {
+	    return m_quickScan;
+	}
+
+	void setPrefSize(const int& _value) {
+	    m_prefSize = _value;
+	}
+	
+
+	int getPrefSize() const {
+	    return m_prefSize;
+	}
+
+	void setPrefK3b(bool _value) {
+	    m_prefK3b = _value;
+	}
+	
+
+	bool getPrefK3b() const {
+	    return m_prefK3b;
+	}
+
+	void setPrefMenu(bool _value) {
+	    m_prefMenu = _value;
+	}
+	
+
+	bool getPrefMenu() const {
+	    return m_prefMenu;
+	}
+
+	void setPrefAutoBurn(bool _value) {
+	    m_prefAutoBurn = _value;
+	}
+	
+
+	bool getPrefAutoBurn() const {
+	    return m_prefAutoBurn;
+	}
+
+	void setPrefMp4Codec(const int& _value) {
+	    m_prefMp4Codec = _value;
+	}
+	
+
+	int getPrefMp4Codec() const {
+	    return m_prefMp4Codec;
+	}
+
+	void setPrefMp4Size(const int& _value) {
+	    m_prefMp4Size = _value;
+	}
+	
+
+	int getPrefMp4Size() const {
+	    return m_prefMp4Size;
+	}
+
+	void setPrefMp4NumberCD(const int& _value) {
+	    m_prefMp4NumberCD = _value;
+	}
+	
+
+	int getPrefMp4NumberCD() const {
+	    return m_prefMp4NumberCD;
+	}
+
+	void setPrefMp4Width(const QString& _value) {
+	    m_prefMp4Width = _value;
+	}
+	
+
+	QString getPrefMp4Width() const {
+	    return m_prefMp4Width;
+	}
+
+	void setPrefMp4Height(const QString& _value) {
+	    m_prefMp4Height = _value;
+	}
+	
+
+	QString getPrefMp4Height() const {
+	    return m_prefMp4Height;
+	}
+
+	void setPrefMp4AudioBitrate(const QString& _value) {
+	    m_prefMp4AudioBitrate = _value;
+	}
+	
+
+	QString getPrefMp4AudioBitrate() const {
+	    return m_prefMp4AudioBitrate;
+	}
+
+	void setCodecAudio(const QStringList& _value) {
+	    m_codecAudio = _value;
+	}
+	
+
+	QStringList getCodecAudio() const {
+	    return m_codecAudio;
+	}
+
+	void setCodecLabels(const QStringList& _value) {
+	    m_codecLabels = _value;
+	}
+	
+
+	QStringList getCodecLabels() const {
+	    return m_codecLabels;
+	}
+
+	void setCodecVideo(const QStringList& _value) {
+	    m_codecVideo = _value;
+	}
+	
+
+	QStringList getCodecVideo() const {
+	    return m_codecVideo;
+	}
+
+	void setPrefCodecAudio(const QString& _value) {
+	    m_prefCodecAudio = _value;
+	}
+	
+
+	QString getPrefCodecAudio() const {
+	    return m_prefCodecAudio;
+	}
+
+	void setPrefCodecVideo(const QString& _value) {
+	    m_prefCodecVideo = _value;
+	}
+	
+
+	QString getPrefCodecVideo() const {
+	    return m_prefCodecVideo;
+	}
+
+	void setPrefCodecLabel(const QString& _value) {
+	    m_prefCodecLabel = _value;
+	}
+	
+
+	QString getPrefCodecLabel() const {
+	    return m_prefCodecLabel;
+	}
+
+	void setDevices(const QStringList& _value) {
+	    m_devices = _value;
+	}
+	
+
+	QStringList getDevices() const {
+	    return m_devices;
+	}
+
+	void setDevicesLabels(const QStringList& _value) {
+	    m_devicesLabels = _value;
+	}
+	
+
+	QStringList getDevicesLabels() const {
+	    return m_devicesLabels;
+	}
+
+	void setDevicesIO(const QStringList& _value) {
+	    m_devicesIO = _value;
+	}
+	
+
+	QStringList getDevicesIO() const {
+	    return m_devicesIO;
+	}
+
+	void setUseMplayer(bool _value) {
+	    m_useMplayer = _value;
+	}
+	
+
+	bool getUseMplayer() const {
+	    return m_useMplayer;
+	}
+
+	void setUseGL(bool _value) {
+	    m_useGL = _value;
+	}
+	
+
+	bool getUseGL() const {
+	    return m_useGL;
+	}
+
+	void setPrefMp4AspectRatio(bool _value) {
+	    m_prefMp4AspectRatio = _value;
+	}
+	
+
+	bool getPrefMp4AspectRatio() const {
+	    return m_prefMp4AspectRatio;
+	}
+
+	void setPrefMp42Passes(bool _value) {
+	    m_prefMp42Passes = _value;
+	}
+	
+
+	bool getPrefMp42Passes() const {
+	    return m_prefMp42Passes;
+	}
+
+	void setMplayerVout(const int& _value) {
+	    m_MplayerVout = _value;
+	}
+	
+
+	int getMplayerVout() const {
+	    return m_MplayerVout;
+	}
+
+	void setMplayerAout(const int& _value) {
+	    m_MplayerAout = _value;
+	}
+	
+
+	int getMplayerAout() const {
+	    return m_MplayerAout;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+};
+
+#endif
