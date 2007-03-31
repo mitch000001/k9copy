8fb4635ea8a4944d2393196745d7fdfd03d15998
jnmchlptt <jnmchlptt@250c61c0-cc0d-0410-af45-d4f8fccfe43b>
2007-03-31 08:27:41 +0000

    git-svn-id: https://k9copy.svn.sourceforge.net/svnroot/k9copy/trunk@192 250c61c0-cc0d-0410-af45-d4f8fccfe43b

--- k9devices/k9haldevice.cpp
+++ k9devices/k9haldevice.cpp
@@ -15,7 +15,7 @@
 #define DBUS_API_SUBJECT_TO_CHANGE
 #include <dbus/dbus.h>
 #include <hal/libhal.h>
-#include <hal/libhal-storage.h>  
+#include <hal/libhal-storage.h>
 
 k9HalDevice::k9HalDevice(QObject *parent, const char *udi)
         : QObject(parent, udi) {
@@ -31,22 +31,24 @@ QString k9HalDevice::volumeName() {
     LibHalDrive *drive= libhal_drive_from_udi ((LibHalContext *)m_connection->m_context, name());
     int num_volumes;
     if (drive !=NULL) {
-	char** volumes = libhal_drive_find_all_volumes ((LibHalContext *)m_connection->m_context, drive, &num_volumes);
-	if (volumes != NULL)
-		for (int i = 0; i < num_volumes; i++) {
-		char *volume_udi;
-		LibHalVolume *volume;
-		volume_udi = volumes[i];
-		QString s(volume_udi);
-		m_volumeUdi=s;
-		volume = libhal_volume_from_udi ((LibHalContext *)m_connection->m_context, volume_udi);
-		if (volume != NULL) {
-			sVol=QString(libhal_volume_get_label (volume));            
-			libhal_volume_free (volume);
-		}
-		libhal_free_string_array (volumes);
-	}
-	libhal_drive_free(drive);
+        char** volumes = libhal_drive_find_all_volumes ((LibHalContext *)m_connection->m_context, drive, &num_volumes);
+        if (volumes != NULL) {
+            for (int i = 0; i < num_volumes; i++) {
+                char *volume_udi;
+                LibHalVolume *volume;
+                volume_udi = volumes[i];
+                QString s(volume_udi);
+                m_volumeUdi=s;
+                volume = libhal_volume_from_udi ((LibHalContext *)m_connection->m_context, volume_udi);
+                if (volume != NULL) {
+                    sVol=QString(libhal_volume_get_label (volume));
+                    libhal_volume_free (volume);
+                }
+            }
+            if (num_volumes >0)
+            	libhal_free_string_array (volumes);
+	}        
+        libhal_drive_free(drive);
     }
     return sVol;
 }
@@ -55,30 +57,31 @@ QString k9HalDevice::mountPoint() {
     QString sMountPoint("");
     LibHalDrive *drive= libhal_drive_from_udi ((LibHalContext *)m_connection->m_context, name());
     if (drive !=NULL) {
-	int num_volumes;
-	char** volumes = libhal_drive_find_all_volumes ((LibHalContext *)m_connection->m_context, drive, &num_volumes);
-	if (volumes != NULL) {
-	    for (int i = 0; i < num_volumes; i++) {
-		char *volume_udi;
-		LibHalVolume *volume;
-		volume_udi = volumes[i];
-		volume = libhal_volume_from_udi ((LibHalContext *)m_connection->m_context, volume_udi);
-		if (volume != NULL) {
-			sMountPoint=QString(libhal_volume_get_mount_point (volume));        
-			libhal_volume_free (volume);
-		}
-	    }
-	    libhal_free_string_array (volumes);
-	}
-	libhal_drive_free(drive);
+        int num_volumes;
+        char** volumes = libhal_drive_find_all_volumes ((LibHalContext *)m_connection->m_context, drive, &num_volumes);
+        if (volumes != NULL) {
+            for (int i = 0; i < num_volumes; i++) {
+                char *volume_udi;
+                LibHalVolume *volume;
+                volume_udi = volumes[i];
+                volume = libhal_volume_from_udi ((LibHalContext *)m_connection->m_context, volume_udi);
+                if (volume != NULL) {
+                    sMountPoint=QString(libhal_volume_get_mount_point (volume));
+                    libhal_volume_free (volume);
+                }
+            }
+            if (num_volumes >0)
+            	libhal_free_string_array (volumes);
+        }
+        libhal_drive_free(drive);
     }
     return sMountPoint;
 }
 
 
 void k9HalDevice::updateVolumeName() {
-   m_volumeName=volumeName();
-   emit volumeChanged(this->getDeviceName(),m_volumeName);
+    m_volumeName=volumeName();
+    emit volumeChanged(this->getDeviceName(),m_volumeName);
 }
 
 void k9HalDevice::getDriveProperties() {
@@ -92,7 +95,7 @@ void k9HalDevice::getDriveProperties() {
     m_canReadDvd=(caps & LIBHAL_DRIVE_CDROM_CAPS_DVDROM)==LIBHAL_DRIVE_CDROM_CAPS_DVDROM;
     m_canBurnDvd=(caps & LIBHAL_DRIVE_CDROM_CAPS_DVDR)==LIBHAL_DRIVE_CDROM_CAPS_DVDR;
     m_model=QString(libhal_drive_get_model(drive));
-//    qDebug(QString("canReadDvd:%1 canBurnDvd:%2 model:%3").arg(m_canReadDvd).arg(m_canBurnDvd).arg(m_model));
+    //    qDebug(QString("canReadDvd:%1 canBurnDvd:%2 model:%3").arg(m_canReadDvd).arg(m_canBurnDvd).arg(m_model));
 
 
     libhal_drive_free(drive);
